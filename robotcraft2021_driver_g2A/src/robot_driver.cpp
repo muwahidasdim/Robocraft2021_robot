7326eefd8ed7529c4a536285ff8fae36c416fd7b
muwahidasdim <64785354+muwahidasdim@users.noreply.github.com>
2021-08-27 12:03:44 +0100

    Update robot_driver.cpp

--- robotcraft2021_driver_g2A/src/robot_driver.cpp
+++ robotcraft2021_driver_g2A/src/robot_driver.cpp
@@ -1,3 +1,4 @@
+
 #include "ros/ros.h"
 #include "geometry_msgs/Pose2D.h"
 #include "geometry_msgs/Twist.h"
@@ -52,6 +53,8 @@ geometry_msgs::Quaternion odom_quat;
     odom_msg.pose.pose.position.z = 0;
     odom_msg.pose.pose.orientation = odom_quat;//odom_msg.pose.pose.orientation=msg.theta;//function to convert theta
 
+        
+
        
 
 
@@ -60,54 +63,54 @@ geometry_msgs::Quaternion odom_quat;
     
    
 }
-
+sensor_msgs::Range rangeMsg_F;
 void callback_F(const std_msgs::Float32 &msg )
 {
-    sensor_msgs::Range rangeMsg;	
-	rangeMsg.header.stamp = ros::Time::now();
-	rangeMsg.header.frame_id = "/front_ir";
-	rangeMsg.radiation_type = 1,                     
-	rangeMsg.field_of_view = 0.034906585;
-	rangeMsg.min_range = 0.1;
-	rangeMsg.max_range = 0.8;
-	rangeMsg.range = msg.data/100.0;
+    	
+	rangeMsg_F.header.stamp = ros::Time::now();
+	rangeMsg_F.header.frame_id = "/front_ir";
+	rangeMsg_F.radiation_type = 1,                     
+	rangeMsg_F.field_of_view = 0.034906585;
+	rangeMsg_F.min_range = 0.1;
+	rangeMsg_F.max_range = 0.8;
+	rangeMsg_F.range = msg.data/100.0;
 
     
-    ir_front_sensor.publish(rangeMsg);
+    ir_front_sensor.publish(rangeMsg_F);
    
    
 }
-
+sensor_msgs::Range rangeMsg_L;
 void callback_L(const std_msgs::Float32 &msg )
 {
-    sensor_msgs::Range rangeMsg;	
-	rangeMsg.header.stamp = ros::Time::now();
-	rangeMsg.header.frame_id = "/left_ir";
-	rangeMsg.radiation_type = 1,                     
-	rangeMsg.field_of_view = 0.034906585;
-	rangeMsg.min_range = 0.1;
-	rangeMsg.max_range = 0.8;
-	rangeMsg.range = msg.data;
+    	
+	rangeMsg_L.header.stamp = ros::Time::now();
+	rangeMsg_L.header.frame_id = "/left_ir";
+	rangeMsg_L.radiation_type = 1,                     
+	rangeMsg_L.field_of_view = 0.034906585;
+	rangeMsg_L.min_range = 0.1;
+	rangeMsg_L.max_range = 0.8;
+	rangeMsg_L.range = msg.data/100.0;
 
 
-    ir_left_sensor.publish(rangeMsg);
+    ir_left_sensor.publish(rangeMsg_L);
     
    
 }
-
+sensor_msgs::Range rangeMsg_R;
 void callback_R(const std_msgs::Float32 &msg )
 {
-    sensor_msgs::Range rangeMsg;	
-	rangeMsg.header.stamp = ros::Time::now();
-	rangeMsg.header.frame_id = "/right_ir";
-	rangeMsg.radiation_type = 1,                     
-	rangeMsg.field_of_view = 0.034906585;
-	rangeMsg.min_range = 0.1;
-	rangeMsg.max_range = 0.8;
-	rangeMsg.range = msg.data;
+    	
+	rangeMsg_R.header.stamp = ros::Time::now();
+	rangeMsg_R.header.frame_id = "/right_ir";
+	rangeMsg_R.radiation_type = 1,                     
+	rangeMsg_R.field_of_view = 0.034906585;
+	rangeMsg_R.min_range = 0.1;
+	rangeMsg_R.max_range = 0.8;
+	rangeMsg_R.range = msg.data/100.0;
 
 
-    ir_right_sensor.publish(rangeMsg);
+    ir_right_sensor.publish(rangeMsg_R);
     
    
 }
@@ -128,23 +131,31 @@ int main(int argc, char **argv){
     ir_front_sensor = n.advertise<sensor_msgs::Range>("ir_front_sensor",5);
     ir_left_sensor = n.advertise<sensor_msgs::Range>("ir_left_sensor",5);
     ir_right_sensor = n.advertise<sensor_msgs::Range>("ir_right_sensor",5);
-    cmd_vel_msg =n.advertise<geometry_msgs::Twist>("cmd_vel",100);
-    ros::Rate loop_rate(10);
+    cmd_vel_msg =n.advertise<geometry_msgs::Twist>("cmd_vel",1000);
+    ros::Rate loop_rate(2);
 
     while(ros::ok){
 
-        //ROS_ERROR("starting publish");
-            velCommand.linear.x = 1.0;
-            velCommand.angular.z = 0.0;
-             pos_x_test=odom_msg.pose.pose.position.x-pos_x;
+        
+       //ROS_ERROR("starting publish");
+            
+	velCommand.linear.x = 0.2;
+            velCommand.angular.z = -1.5;	            
+	 //pos_x_test=odom_msg.pose.pose.position.x-pos_x;
+	
 
         
-    
-        if(pos_x_test>=0.05)
+    	/*if(rangeMsg_F.range<=2.0)
+	{
+		velCommand.linear.x = 0.3;
+            velCommand.angular.z = 1.0;
+		ROS_ERROR("starting publish");
+	}
+       /* if(pos_x_test>=0.30)
         {
             velCommand.linear.x=0;
             velCommand.angular.z=0;
-            /*float pos_z_test=0;
+            float pos_z_test=0;
             float pos_z=msg.theta;
             
 
@@ -160,10 +171,85 @@ int main(int argc, char **argv){
             velCommand.angular.z =0;
             
             pos_x_test=0;
-            pos_x=msg.x;*/
+            pos_x=odom_msg.pose.pose.position.x;
+        }*/
+	if( rangeMsg_R.range<1.5)
+      {
+        
+        if(rangeMsg_R.range<1.0 && rangeMsg_F.range>=2.5)
+        {
+          velCommand.linear.x=0.3;
+          velCommand.angular.z=-1.0;
+
+          ROS_INFO("1");
+        }
+        else if(rangeMsg_F.range>=2.5)
+        {
+          
+           velCommand.linear.x=0.2;
+          velCommand.angular.z=-1.0;
+          ROS_INFO("22"); 
         }
         
-       
+        else if(rangeMsg_F.range<2.5)
+        {
+          
+           velCommand.linear.x=0.2;
+            velCommand.angular.z=-1.5;
+
+            ROS_INFO("3");
+         }
+      }
+      else if(rangeMsg_R.range>2.0 )
+      {
+        if(rangeMsg_F.range>=2.0 )
+        {
+          velCommand.linear.x=0.4;
+          velCommand.angular.z=1.0;
+
+          ROS_INFO("4"); 
+        }
+        else if(rangeMsg_F.range<2.0)
+        {
+          velCommand.linear.x=0.2;
+          velCommand.angular.z=0.9;
+
+          ROS_INFO("5");
+        }
+        
+      }
+      else if(rangeMsg_R.range<=1.5 && rangeMsg_L.range<=2.0)
+      {
+        if(rangeMsg_R.range<1.0 && rangeMsg_F.range>=2.5)
+        {
+          velCommand.linear.x=0.2;
+          velCommand.angular.z=-1.0;
+
+          ROS_INFO("6");
+        }
+        else if(rangeMsg_F.range<2.5)
+        {
+          velCommand.linear.x=0.2;
+          velCommand.angular.z=-1.0;
+
+          ROS_INFO("7");
+        }
+        else if(rangeMsg_F.range>=2.5)
+        {
+          velCommand.linear.x=1.0;
+          velCommand.angular.z=0.0;
+
+          ROS_INFO("8");
+        }
+      }
+      else if(rangeMsg_R.range>2.5)
+      {
+
+          velCommand.linear.x=0.3;
+          velCommand.angular.z=1.0;
+          ROS_INFO("9");
+          
+      }
 
         cmd_vel_msg.publish(velCommand);
 
@@ -174,4 +260,3 @@ int main(int argc, char **argv){
 
     return 0;
 }
-
